f7d23d0ca3fe3372b9b53306e495f5708ac8cc1c
Hugo Burton <burto732@gmail.com>
2023-08-02 10:49:45 +1000

    --

--- julia cupy.py
+++ julia cupy.py
@@ -1,40 +1,45 @@
-import cupy as cp
+import torch
 import matplotlib.pyplot as plt
 
 # Function to generate the Julia set
 def julia_set(width, height, real_min, real_max, imag_min, imag_max, c_real, c_imag, max_iterations=100, escape_radius=2.0):
-    # Rest of the code remains the same...
-    # Instead of using torch, we now use cupy
-
     # Create a grid of complex numbers on the GPU
-    real = cp.linspace(real_min, real_max, width)
-    imag = cp.linspace(imag_min, imag_max, height)
-    real, imag = cp.meshgrid(real, imag)
-    c = cp.complex(real, imag)
+    real = torch.linspace(real_min, real_max, width, device='cuda')
+    imag = torch.linspace(imag_min, imag_max, height, device='cuda')
+    real, imag = torch.meshgrid(real, imag)
+    c = torch.complex(real, imag)
 
     # Initialize the output matrix with zeros (not in the set) on the GPU
-    output = cp.zeros_like(c, dtype=cp.uint8)
+    output = torch.zeros_like(c, dtype=torch.uint8, device='cuda')
 
     # Convert c_real and c_imag to tensors on the GPU
-    c_real = cp.array(c_real)
-    c_imag = cp.array(c_imag)
+    c_real = torch.tensor(c_real, device='cuda')
+    c_imag = torch.tensor(c_imag, device='cuda')
 
     # Iterate to determine points in the Julia set
-    z = c.copy()
+    z = c.clone()
     for i in range(max_iterations):
-        z = z * z + cp.complex(c_real, c_imag)
+        z = z * z + torch.complex(c_real, c_imag)
         in_set = (z.abs() <= escape_radius)
         output[in_set] = i + 1
 
     return output
 
-# Rest of the code remains the same...
+# Parameters for the Julia set
+width = 800
+height = 600
+real_min, real_max = -2.0, 2.0
+imag_min, imag_max = -1.5, 1.5
+c_real, c_imag = -0.52535, -0.52535  # Feel free to change these values to explore different Julia sets
+
+# Generate the Julia set on the GPU
+julia = julia_set(width, height, real_min, real_max, imag_min, imag_max, c_real, c_imag)
 
 # Transfer the result back to the CPU for visualization
-julia_cpu = cp.asnumpy(julia)
+julia_cpu = julia.cpu()
 
 # Display the Julia set
-plt.imshow(julia_cpu, cmap='hot', extent=(real_min, real_max, imag_min, imag_max))
+plt.imshow(julia_cpu, cmap='turbo', extent=(real_min, real_max, imag_min, imag_max))
 plt.title("Julia Set")
 plt.xlabel("Real")
 plt.ylabel("Imaginary")
