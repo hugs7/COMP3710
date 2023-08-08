f32e8cbc71b05e0750ae965216e63643d63f5bf7
hugs7 <burto732@gmail.com>
2023-08-08 19:57:12 +1000

    Finalised square fractal. Ensured meshgrid formulation is correct
    fixed issues in recursive call

--- Demo 1/square.py
+++ Demo 1/square.py
@@ -12,80 +12,65 @@ print(torch.cuda.is_available())
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 print()
+
+
 STEP = 1
 
-# torch.arange(start, stop, step) returns 1D tensor
-# x_range = torch.arange(0, 1, STEP)
-# y_range = torch.arange(0, 1, STEP)
-x_len, y_len = 3, 3
-# Torch.mesgrid (Tensor X, Tensor Y)
-# X, Y = torch.meshgrid(x_range, y_range)
-# print(X)
-# print(Y)
-# 3x3 grid to start where 0,0 is the centre
-# so -1 to 1 on x and y
+length = 3**8
 
-# Move meshgrid onto the GPU
-# x = X.to(device)
-# y = Y.to(device)
+# Create x and y ranges using torch.arange
+x_range = torch.arange(-1 * (length - 1) // 2, 1 +
+                       (length - 1) // 2, STEP, device=device)
+y_range = torch.arange(-1 * (length - 1) // 2, 1 +
+                       (length - 1) // 2, STEP, device=device)
 
-# Create zeros array
-# grid_z = torch.zeros_like(X)
-# for i, row in enumerate(grid_z):
-#     for j, cell in enumerate(row):
-#         if (i % 2 == 1 and j % 2 == 0) or (i % 2 == 0 and j % 2 == 1):
-#             grid_z[i][j] = 2
-# print(grid_z)
+# Use torch.meshgrid to create meshgrid tensors
+X, Y = torch.meshgrid(x_range, y_range)
+
+# Create zeros tensor
+new_fractal = torch.zeros((length, length), device=device)
+# new_fractal = torch.zeros((length, length))
+print(new_fractal)
 
 
-def growSquare(x_length, y_length):
-    # Takes n*n meshgrid and makes it 3n*3n
-
-    new_X_t = torch.arange(-3 * (x_length - 1) // 2,
-                           3 * (x_length - 1) // 2, STEP)
-    new_Y_t = torch.arange(-3 * (y_length - 1) // 2,
-                           3 * (y_length - 1) // 2, STEP)
-
-    # Make new grid
-    new_Y, new_X = torch.meshgrid(new_X_t, new_Y_t)
-
-    # Create a tensor to hold the fractal values
-    fractal = torch.zeros_like(new_X)
-    return fractal
-
-
-def populate_grid(fractal_grid, length):
+def populate_grid(grid, length):
     if length == 3:  # base case
         # Code to populate the base case (e.g., filling specific cells)
-        for i, row in enumerate(fractal_grid):
-            for j, cell in enumerate(row):
-                if (i % 2 == 1 and j % 2 == 0) or (i % 2 == 0 and j % 2 == 1):
-                    fractal_grid[i][j] = 1
-        print(fractal_grid)
+        for row in range(3):
+            for col in range(3):
+                if (row % 2 == 1 and col % 2 == 0) or (row % 2 == 0 and col % 2 == 1) or (row == 1 and col == 1):
+                    grid[row, col] = 1
+                # else:
+                #     grid[row, col] = 1
 
-        return fractal_grid
+        return grid
     else:
         # recursive case
         new_length = length // 3
         for row in range(3):
-            for supercell in range(3):
-                if (row % 2 == 1 and supercell % 2 == 0) or (row % 2 == 0 and supercell % 2 == 1):
-                    sub_grid = torch.zeros(
-                        new_length, new_length, dtype=fractal_grid.dtype)
+            # print("Row", row)
+            for col in range(3):
+                # print("Col", col)
+
+                if (row % 2 == 1 and col % 2 == 0) or (row % 2 == 0 and col % 2 == 1) or (row == 1 and col == 1):
+
+                    sub_row = (row * length) // 3
+                    sub_col = (col * length) // 3
+
+                    sub_grid = grid[sub_row: sub_row +
+                                    new_length, sub_col: sub_col + new_length]
                     populate_grid(sub_grid, new_length)
                     # Now you have a sub-grid filled with the fractal pattern, and you can place it in the main grid
-                    fractal_grid[row * new_length: (row + 1) * new_length, supercell *
-                                 new_length: (supercell + 1) * new_length] = sub_grid
+                    # grid[row * new_length: (row + 1) * new_length, supercell *
+                    #              new_length: (supercell + 1) * new_length] = sub_grid
 
 
-for i in range(5):
-    # Test the function
-    new_fractal = growSquare(x_len, y_len)
-    populate_grid(new_fractal, x_len)
-    x_len *= 3
-    y_len *= 3
-    print(x_len, y_len)
+populate_grid(new_fractal, length)
 
-plt.imshow(new_fractal.cpu(), cmap='gray')
+# Calculate extent for centering the plot at 0,0
+extent = [-1 * (length - 1) // 2, (length - 1) // 2, -
+          1 * (length - 1) // 2, (length - 1) // 2]
+
+plt.imshow(new_fractal.cpu(), cmap='gray', extent=extent)
 plt.tight_layout(pad=0)
 plt.show()
